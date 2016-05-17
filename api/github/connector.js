d05817ec4aadc5136703fda188f451c9a77639ca
Sashko Stubailo <sashko@stubailo.com>
2016-05-16 20:51:31 -0700

    Basic code for GitHub connector

---
+++ api/github/connector.js
@@ -0,0 +1,26 @@
+import rp from 'request-promise';
+import DataLoader from 'dataloader';
+
+// Keys are GitHub API URLs, values are { etag, result } objects
+const eTagCache = {};
+
+export class GitHubConnector {
+  constructor() {
+    this.loader = new DataLoader(this.fetch);
+  }
+
+  fetch(urls) {
+    const options = {
+      json: true,
+    };
+
+    // TODO: implement ETags
+
+    return Promise.all(urls.map((url) => {
+      return rp({
+        url: url,
+        ...options,
+      });
+    }));
+  }
+}
