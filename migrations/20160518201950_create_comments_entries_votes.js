9eeadbaab95b0d88f8432496475127b57ed70c09
Sashko Stubailo <sashko@stubailo.com>
2016-05-18 20:28:28 -0700

    Set up migration, gitignore sqlite db

---
+++ migrations/20160518201950_create_comments_entries_votes.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('comments', function (table) {
+      table.increments();
+      table.timestamps();
+      table.string('posted_by');
+      table.text('content');
+      table.integer('entry_id');
+    }),
+
+    knex.schema.createTable('entries', function (table) {
+      table.increments();
+      table.timestamps();
+      table.string('repository_name');
+      table.string('posted_by');
+    }),
+
+    knex.schema.createTable('votes', function (table) {
+      table.increments();
+      table.timestamps();
+      table.integer('vote_value');
+      table.string('username');
+    }),
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('comments'),
+    knex.schema.dropTable('entries'),
+    knex.schema.dropTable('votes'),
+  ]);
+};
