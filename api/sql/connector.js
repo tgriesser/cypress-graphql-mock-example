cd4737b3a9868923c30c84888f19ba677714e1de
Sashko Stubailo <sashko@stubailo.com>
2016-05-25 20:31:54 -0700

    Add comment about why our SQL connector is so minimal

--- api/sql/connector.js
+++ api/sql/connector.js
@@ -1,4 +1,6 @@
 import knex from 'knex';
 import { development } from '../../knexfile';
 
+// Eventually we want to wrap Knex to do some batching and caching, but for
+// now this will do since we know none of our queries need it
 export default knex(development);
