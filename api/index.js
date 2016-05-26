70d8d85b825f3cb424f52e9ef3aca64a2d154a4a
Sashko Stubailo <sashko@stubailo.com>
2016-05-25 20:52:40 -0700

    Add query size limit

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import session from 'express-session';
 import passport from 'passport';
 import { apolloServer } from 'apollo-server';
 import { Strategy as GitHubStrategy } from 'passport-github';
-import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import knex from './sql/connector';
 
 var KnexSessionStore = require('connect-session-knex')(session);
@@ -39,6 +39,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(bodyParser());
+
 app.get('/login/github',
   passport.authenticate('github'));
 
@@ -54,6 +56,16 @@ app.get('/logout', function(req, res){
 });
 
 app.use('/graphql', apolloServer((req) => {
+  // Get the query, the same way express-graphql does it
+  // https://github.com/graphql/express-graphql/blob/3fa6e68582d6d933d37fa9e841da5d2aa39261cd/src/index.js#L257
+  const query = req.query.query || req.body.query;
+  if (query.length > 1000) {
+    console.log(query);
+    // None of our app's queries are this long
+    // Probably indicates someone trying to send an overly expensive query
+    throw new Error('Query too large.');
+  }
+
   let user;
   if (req.user) {
     // We get req.user from passport-github with some pretty oddly named fields,
