a004e9466d792b080a2a818ac4193f888af48d08
Sashko Stubailo <sashko@stubailo.com>
2016-05-26 11:23:27 -0700

    Remove extra log

--- api/index.js
+++ api/index.js
@@ -60,7 +60,6 @@ app.use('/graphql', apolloServer((req) => {
   // https://github.com/graphql/express-graphql/blob/3fa6e68582d6d933d37fa9e841da5d2aa39261cd/src/index.js#L257
   const query = req.query.query || req.body.query;
   if (query && query.length > 2000) {
-    console.log(query);
     // None of our app's queries are this long
     // Probably indicates someone trying to send an overly expensive query
     throw new Error('Query too large.');
