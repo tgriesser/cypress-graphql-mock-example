80ab1734efc4f825f41667585f0a3265e3e33541
amandajliu <ajliu72@gmail.com>
2016-06-29 18:02:21 -0700

    changed CommentsPage back to React class, and unpacked props

--- ui/CommentsPage.js
+++ ui/CommentsPage.js
@@ -11,76 +11,101 @@ function Comment({ username, userUrl, content, createdAt }) {
   );
 }
 
-function CommentsPage({ data, mutations }) {
-  if (data.loading) {
+var CommentsPage = React.createClass ({
+  propTypes: {
+    data: React.PropTypes.shape({
+      loading: React.PropTypes.bool.isRequired,
+      errors: React.PropTypes.array,
+      refetch: React.PropTypes.func,
+      currentUser: React.PropTypes.shape({
+        login: React.PropTypes.string
+      }),
+      entry: React.PropTypes.shape({
+        comments: React.PropTypes.arrayOf(
+          React.PropTypes.shape({
+
+
+            postedBy: React.PropTypes.shape({
+              login: React.PropTypes.string.isRequired
+            }),
+            createdAt: React.PropTypes.number,
+            content: React.PropTypes.string.isRequired
+          })
+        )
+      })
+    })
+  },
+  getDefaultProps: function() {
+    return {
+      comments: []
+    };
+  },
+  _submitForm: function(event){
+    event.preventDefault();
+    const repositoryName = this.props.params.org+"/"+this.props.params.repoName;
+    const commentContent = event.target.newCommentContent.value;
+    this.props.mutations.submitComment(repositoryName, commentContent).then((res) => {
+      if (! res.errors) {
+        document.getElementById('newComment').value='';
+        this.props.data.refetch();
+      }
+    });
+  },
+  render: function(){
+    const {data} = this.props;
+    if (data.loading){
+      return (
+        <div>Loading...</div>
+      );
+    }
+    const repo = data.entry.repository;
+    const {submitComment} = this.props;
     return (
-      <div>Loading...</div>
+      <div>
+        <div>
+          <h1>Comments for <a href={repo.html_url}>{repo.full_name}</a></h1>
+          {data.currentUser && <form onSubmit={this._submitForm}>
+            <div className="form-group">
+
+              <input
+                type="text"
+                className="form-control"
+                id="newComment"
+                name="newCommentContent"
+                placeholder="Write your comment here!"
+              />
+            </div>
+
+            {submitComment.errors && (
+              <div className="alert alert-danger" role="alert">
+                {submitComment.errors[0].message}
+              </div>
+            )}
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>}
+          {!data.currentUser && <div><em>Log in to comment.</em></div>}
+        </div>
+        <br/>
+        <div>
+          <div> {
+            data.entry.comments.map((comment) => (
+              <Comment
+                key={comment.postedBy.login + comment.content + comment.createdAt + repo.full_name}
+                username={comment.postedBy.login} 
+                content={comment.content} 
+                createdAt={comment.createdAt}
+                userUrl={comment.postedBy.html_url}
+              />
+            ))
+          }</div>
+
+        </div>
+      </div>
     );
   }
-  const repoName = data.entry.repository.full_name;
-  function submitForm(event) {
-    event.preventDefault();
-    const repositoryName = data.entry.repository.full_name;
-    const commentContent = event.target.newCommentContent.value;
-    if (! commentContent) {
-      document.getElementById('noCommentContent').className = 'alert alert-danger';
-    } else {
-      mutations.submitComment(repositoryName, commentContent).then((res) => {
-        if (! res.errors) {
-          document.getElementById('newComment').value = '';
-          data.refetch();
-        }
-      });
-    }
-  }
-
-  return (
-    <div>
-      <div>
-        <h1>Comments for <a href={data.entry.repository.html_url}>{repoName}</a></h1>
-        {data.currentUser && <form onSubmit={submitForm}>
-          <div className="form-group">
-
-            <input
-              type="text"
-              className="form-control"
-              id="newComment"
-              name="newCommentContent"
-              placeholder="Write your comment here!"
-            />
-          </div>
-
-          {mutations.submitComment.errors && (
-            <div className="alert alert-danger" role="alert">
-              {mutations.submitComment.errors[0].message}
-            </div>
-          )}
-          <div className="alert alert-danger hidden" role="alert" id="noCommentContent">
-            Comment must have content.
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>}
-        {!data.currentUser && <div><em>Log in to comment.</em></div>}
-      </div> <br/>
-      <div>
-        <div> {
-          data.entry.comments.map((comment) => (
-            <Comment
-              key={comment.postedBy.login + comment.content + comment.createdAt + repoName}
-              username={comment.postedBy.login}
-              content={comment.content}
-              createdAt={comment.createdAt}
-              userUrl={comment.postedBy.html_url}
-            />
-          ))
-        }</div>
-
-      </div>
-    </div>
-  );
-}
+});
 const CommentWithData = connect({
   mapQueriesToProps: ({ ownProps }) => ({
     data: {
