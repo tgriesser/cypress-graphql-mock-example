527ac8013c29dfe0991b08c9145f2d665cb7c5d2
Dhaivat Pandya <dhaivat@meteor.com>
2017-01-04 12:30:03 -0800

    updated the webpack config and fixed a dep in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,59 @@
-module.exports = {
-  entry: './ui/client.js',
-  output: {
-    filename: "bundle.js",
-    publicPath: '/'
+module.exports = [
+  {
+    name: 'browser',
+    entry: './ui/client.js',
+    output: {
+      filename: "bundle.js",
+      publicPath: '/'
+    },
+    module: {
+      loaders: [{
+        test: /\.css/,
+        loader: 'style!css'
+      }, {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /(node_modules)/
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.(graphql|gql)$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader'
+      }]
+    },
+    devServer: {
+    },
+    devtool: "eval-source-map",
   },
-  module: {
-    loaders: [{
-      test: /\.css/,
-      loader: 'style!css'
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /(node_modules)/
-    }, {
-      test: /\.json$/,
-      loader: 'json'
-    }]
-  },
-  devServer: {
-  },
-  devtool: "eval-source-map",
-}
+  {
+    name: 'SSR server',
+    entry: './ui/server.js',
+    target: 'node',
+    output: {
+      filename: 'server_bundle.js',
+      publicPath: '/',
+    },
+    libraryTarget: 'commonjs32',
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          exclude: /(node_modules)/
+        },
+        {
+          test: /\.json$/,
+          loader: 'json'
+        },
+        {
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          loader: 'graphql-tag/loader'
+        }
+      ]
+    }
+  }
+]
