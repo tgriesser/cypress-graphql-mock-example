80765ebe19d5572830f0fd0b2da9494208ae8efa
Dhaivat Pandya <dhaivat@meteor.com>
2017-01-09 09:37:40 -0800

    updated to use the new production option on extractgql's network interface'

--- ui/transport.js
+++ ui/transport.js
@@ -7,23 +7,13 @@ import config from './config';
 // persisted query network interface (from `extractgql`) depending on
 // the configuration within `./config.js.`
 export default function getNetworkInterface(apiUrl = '/graphql', headers = {}) {
-  const persistedQueryNI = new PersistedQueryNetworkInterface({
+  return new PersistedQueryNetworkInterface({
     queryMap,
     uri: apiUrl,
     opts: {
       credentials: 'same-origin',
       headers,
     },
+    production: config.persistedQueries,
   });
-
-  const standardNetworkInterface = createNetworkInterface({
-    uri: apiUrl,
-    opts: {
-      credentials: 'same-origin',
-      headers,
-    },
-    transportBatching: true,
-  });
-
-  return (config.persistedQueries ? persistedQueryNI : standardNetworkInterface);
 }
