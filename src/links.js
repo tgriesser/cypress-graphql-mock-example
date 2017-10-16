c6eb678bee8bbd6fbc8b63b55b981c6b62343219
Peggy Rayzis <peggyrayzis@gmail.com>
2017-10-16 15:34:18 -0400

    fix(error link): added comments, cleaned up logging fns

--- src/links.js
+++ src/links.js
@@ -1,17 +1,27 @@
 import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
-import WSLink from 'apollo-link-ws';
+import { WebSocketLink } from 'apollo-link-ws';
 import { onError } from 'apollo-link-error';
 import { getMainDefinition } from 'apollo-utilities';
 
-// TODO (PEGGY): change to graphQLErrors when james gives you the ok
-export const errorLink = onError(({ graphqlErrors, networkError }) => {
-  if (graphqlErrors) console.log(`[GraphQL errors]: ${graphqlErrors}`);
+export const errorLink = onError(({ graphQLErrors, networkError }) => {
+  /*
+  onError receives a callback in the event a GraphQL or network error occurs.
+  This example is a bit contrived, but in the real world, you could connect
+  a logging service to the errorLink or perform a specific action in response
+  to an error.
+  */
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${location}, Path: ${path}`
+      )
+    );
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
 export const subscriptionLink = (config = {}) =>
-  new WSLink({
+  new WebSocketLink({
     uri:
       process.env.NODE_ENV !== 'production'
         ? 'ws://localhost:3010/subscriptions'
@@ -35,7 +45,7 @@ export const queryOrMutationLink = (config = {}) =>
   }).concat(
     new HttpLink({
       ...config,
-    }),
+    })
   );
 
 export const requestLink = ({ queryOrMutationLink, subscriptionLink }) =>
@@ -50,5 +60,5 @@ export const requestLink = ({ queryOrMutationLink, subscriptionLink }) =>
       return kind === 'OperationDefinition' && operation === 'subscription';
     },
     subscriptionLink,
-    queryOrMutationLink,
+    queryOrMutationLink
   );
