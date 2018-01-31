c3a4bc4cf92f27572f99f440eea64c16cf8c5335
James Baxley <james@meteor.com>
2018-01-31 14:33:43 -0500

    remove dead react code

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -40,7 +40,7 @@ module.exports = {
         NODE_ENV: JSON.stringify(DEV ? 'development' : 'production'),
       },
     }),
-    DEV &&
+    !DEV &&
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           screw_ie8: true, // React doesn't support IE8
