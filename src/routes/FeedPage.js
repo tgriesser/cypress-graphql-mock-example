39ee227969af4706160286b2c9292137f86a107c
James Baxley <james@meteor.com>
2018-03-18 21:11:18 -0400

    replace FeedPage withUser using Query

--- src/routes/FeedPage.js
+++ src/routes/FeedPage.js
@@ -17,64 +17,59 @@ class FeedPage extends React.Component {
   }
 
   render() {
-    const { vote, currentUser, fetchMore, match } = this.props;
+    const { vote, fetchMore, match } = this.props;
 
     return (
-      <Query
-        query={FEED_QUERY}
-        variables={{
-          type:
-            (match.params &&
-              match.params.type &&
-              match.params.type.toUpperCase()) ||
-            'TOP',
-          offset: 0,
-          limit: ITEMS_PER_PAGE,
-        }}
-        fetchPolicy="cache-and-network"
-      >
-        {({ loading, data, fetchMore }) => {
-          return (
-            <div>
-              <Feed
-                entries={data.feed || []}
-                loggedIn={!!this.props.currentUser}
-                onVote={vote}
-                onLoadMore={() =>
-                  fetchMore({
-                    variables: {
-                      offset: data.feed.length,
-                    },
-                    updateQuery: (prev, { fetchMoreResult }) => {
-                      if (!fetchMoreResult) return prev;
-                      return Object.assign({}, prev, {
-                        feed: [...prev.feed, ...fetchMoreResult.feed],
-                      });
-                    },
-                  })
-                }
-              />
-              {loading ? <Loading /> : null}
-            </div>
-          );
-        }}
+      <Query query={CURRENT_USER_QUERY}>
+        {({ data: { currentUser } = {} }) => (
+          <Query
+            query={FEED_QUERY}
+            variables={{
+              type:
+                (match.params &&
+                  match.params.type &&
+                  match.params.type.toUpperCase()) ||
+                'TOP',
+              offset: 0,
+              limit: ITEMS_PER_PAGE,
+            }}
+            fetchPolicy="cache-and-network"
+          >
+            {({ loading, data, fetchMore }) => {
+              return (
+                <div>
+                  <Feed
+                    entries={data.feed || []}
+                    loggedIn={!!this.props.currentUser}
+                    onVote={vote}
+                    onLoadMore={() =>
+                      fetchMore({
+                        variables: {
+                          offset: data.feed.length,
+                        },
+                        updateQuery: (prev, { fetchMoreResult }) => {
+                          if (!fetchMoreResult) return prev;
+                          return Object.assign({}, prev, {
+                            feed: [...prev.feed, ...fetchMoreResult.feed],
+                          });
+                        },
+                      })
+                    }
+                  />
+                  {loading ? <Loading /> : null}
+                </div>
+              );
+            }}
+          </Query>
+        )}
       </Query>
     );
   }
 }
 
 FeedPage.propTypes = {
-  currentUser: PropTypes.shape({
-    login: PropTypes.string.isRequired,
-  }),
   vote: PropTypes.func.isRequired,
 };
-
-const withUser = graphql(CURRENT_USER_QUERY, {
-  props: ({ data }) => ({
-    currentUser: data && data.currentUser,
-  }),
-});
 
 const withMutations = graphql(VOTE_MUTATION, {
   props: ({ mutate }) => ({
@@ -85,4 +80,4 @@ const withMutations = graphql(VOTE_MUTATION, {
   }),
 });
 
-export default withMutations(withUser(FeedPage));
+export default withMutations(FeedPage);
