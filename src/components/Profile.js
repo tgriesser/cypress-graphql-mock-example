d82a88cb10032489871566ce8eb5fb01a8832562
James Baxley <james@meteor.com>
2018-03-18 22:41:00 -0400

    fix lint error

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,35 +12,37 @@ const PROFILE_QUERY = gql`
   }
 `;
 
-export default () => (
-  <Query query={PROFILE_QUERY} fetchPolicy="cache-and-network">
-    {({ loading, data: { currentUser } }) => {
-      if (loading) {
-        return <p className="navbar-text navbar-right">Loading...</p>;
-      } else if (currentUser) {
+export default function Profile() {
+  return (
+    <Query query={PROFILE_QUERY} fetchPolicy="cache-and-network">
+      {({ loading, data: { currentUser } }) => {
+        if (loading) {
+          return <p className="navbar-text navbar-right">Loading...</p>;
+        } else if (currentUser) {
+          return (
+            <span>
+              <p className="navbar-text navbar-right">
+                {currentUser.login}
+                &nbsp;
+                <a href="/logout">Log out</a>
+              </p>
+              <Link
+                type="submit"
+                className="btn navbar-btn navbar-right btn-success"
+                to="/submit"
+              >
+                <span className="glyphicon glyphicon-plus" aria-hidden="true" />
+                &nbsp; Submit
+              </Link>
+            </span>
+          );
+        }
         return (
-          <span>
-            <p className="navbar-text navbar-right">
-              {currentUser.login}
-              &nbsp;
-              <a href="/logout">Log out</a>
-            </p>
-            <Link
-              type="submit"
-              className="btn navbar-btn navbar-right btn-success"
-              to="/submit"
-            >
-              <span className="glyphicon glyphicon-plus" aria-hidden="true" />
-              &nbsp; Submit
-            </Link>
-          </span>
+          <p className="navbar-text navbar-right">
+            <a href="/login/github">Log in with GitHub</a>
+          </p>
         );
-      }
-      return (
-        <p className="navbar-text navbar-right">
-          <a href="/login/github">Log in with GitHub</a>
-        </p>
-      );
-    }}
-  </Query>
-);
+      }}
+    </Query>
+  );
+}
